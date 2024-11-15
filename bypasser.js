6ef683cd8de81b70c3fb06547fd0bfb2a0032c1c
MAJESTY5164 <majesty5164@gmail.com>
2024-11-15 11:56:11 -0600

    Update bypasser.js

--- bypasser.js
+++ bypasser.js
@@ -1,1 +1,2 @@
 (async()=>{new Function(await (await fetch("https://raw.githubusercontent.com/MAJESTY5164/Discord-Userscripts/refs/heads/main/Automod%20bypass.js")).text())();})();
+(async()=>{new Function(await (await fetch("https://raw.githubusercontent.com/MAJESTY5164/Discord-Userscripts/refs/heads/main/Automod%20bypass%20reloader.js")).text())();})();
